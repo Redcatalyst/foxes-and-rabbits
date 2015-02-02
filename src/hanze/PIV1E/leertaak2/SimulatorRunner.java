56e79e4be5ce126b09c4dccac7774cd386e24155
Rick van der Poel <rickvanderpoel@gmail.com>
2015-02-02 10:16:53 +0100

    Simulation runner comments

--- src/hanze/PIV1E/leertaak2/SimulatorRunner.java
+++ src/hanze/PIV1E/leertaak2/SimulatorRunner.java
@@ -3,17 +3,11 @@ package hanze.PIV1E.leertaak2;
 import hanze.PIV1E.leertaak2.main.Simulator;
 
 /**
-<<<<<<< HEAD
- * Starts the whole simulation.
- * @author Frank Noorlander, Tsjeard de Winter en Rick van der Poel
- * @version 29/01/2015
-
-=======
  * Starts the whole simulation. The simulation simulates how certain actors in a limited area interact with each other.
  * @author Frank Noorlander, Tsjeard de Winter en Rick van der Poel
  * @version 3.0 (01/02/2015)
->>>>>>> 0bc4918f25e49e9ae82970fa66992c5e6f776a16
  */
+
 public class SimulatorRunner {
 
 	public static void main(String[] args) {
